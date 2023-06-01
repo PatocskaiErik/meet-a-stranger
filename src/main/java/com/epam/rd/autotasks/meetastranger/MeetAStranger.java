230050f18067b1d3b98c33ed5b4f6e22d19236b7
Erik Patocskai <patoerik93@gmail.com>
2023-06-01 20:30:32 +0000

    Added scanner

--- src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
+++ src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -1,6 +1,11 @@
 package com.epam.rd.autotasks.meetastranger;
 public class MeetAStranger {
     public static void main(String[] args) {
-        //Write a program, which read a String from System.in and print "Hello, <input string>"
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        System.out.println("Hello, " + input);
     }
 }

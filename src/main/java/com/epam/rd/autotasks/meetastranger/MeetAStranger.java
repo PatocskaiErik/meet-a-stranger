3f684f5fee3766dab2091c88f3db6349f783a172
Erik Patocskai <patoerik93@gmail.com>
2023-06-01 20:37:08 +0000

    imported scanner

--- src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
+++ src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -1,4 +1,7 @@
 package com.epam.rd.autotasks.meetastranger;
+
+import java.util.Scanner;
+
 public class MeetAStranger {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
